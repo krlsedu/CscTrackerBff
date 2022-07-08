de9d3cb04073505711f98e7e5083a763eef416e9
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-07-08 13:57:48 -0300

    Simplify

--- src/main/java/com/csctracker/bff/controller/HeartbeatController.java
+++ src/main/java/com/csctracker/bff/controller/HeartbeatController.java
@@ -1,6 +1,5 @@
 package com.csctracker.bff.controller;
 
-import com.csctracker.bff.dto.HeartbeatDTO;
 import com.csctracker.bff.service.DispachService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.List;
 
 @RestController
 public class HeartbeatController {
@@ -21,10 +19,10 @@ public class HeartbeatController {
     }
 
     @GetMapping(value = "/heartbeats", produces = "application/json")
-    public ResponseEntity<List<HeartbeatDTO>> getHeartbeat(
+    public ResponseEntity<String> getHeartbeat(
             @RequestParam(value = "dateIni", required = false) Date dateIni,
             @RequestParam(value = "dateEnd", required = false) Date dateEnd,
             @RequestParam(value = "period", required = false) String period) {
-        return new ResponseEntity<>(dispachService.dispachList(HeartbeatDTO.class), HttpStatus.OK);
+        return new ResponseEntity<>(dispachService.dispach(), HttpStatus.OK);
     }
 }
